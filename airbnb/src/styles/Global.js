1732e05a985cba5dcbd0239624536b3c671b4190
tkv00 <tkv0098@gmail.com>
2024-12-24 22:08:33 +0900

    SCRUM-1 글로벌 레이아웃 설정

---
+++ airbnb/src/styles/Global.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles=StyleSheet.create({
+    colors:{
+        Rausch:"#FF5A5F",
+        Babu:"#00A699",
+        Arches:"#FC642D",
+        Hof:"#484848",
+        Foggy:"#767676"
+    }
+})
